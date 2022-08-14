8852be2e525a0839c003c6b9b7b2cbc9b54e8ef3
Nicholas <nicholas.nmalinda@gmail.com>
2022-08-14 11:10:41 +0300

    commit tree 2a6061e7557fc402b7ba9fe51a5c65d6636ae19f

---
+++ app.rb
@@ -0,0 +1,21 @@
+#app.rb
+#puts
+puts "Hello World!"
+puts "Hello World!"
+puts "Hello World!"
+
+#print
+print "Hello world!"
+print "Hello world!"
+print "Hello world!"
+ 
+# use p to output non-strings
+#This is the equivalent of calling:
+#puts [1, 2, 3].inspect
+p [1, 2, 3]
+
+#pp "pretty-print" by caliing .pretty_inspect method on our data
+
+pp [{id: 1, hello: "World!"}, {id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}, {id: 4, hello: "Learner"}]
+
+print "Pass this test, please."
